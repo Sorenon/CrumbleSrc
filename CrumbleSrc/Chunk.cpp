ecf76f3bc13049987641c03990b179f293690d99
Charlie Bull <thecreepersfriend@gmail.com>
2019-04-03 15:10:59 +0100

    Added simple world storage

--- CrumbleSrc/Chunk.cpp
+++ CrumbleSrc/Chunk.cpp
@@ -1,10 +1,38 @@
 #include "Chunk.h"
-
-
+#include <algorithm>
+#include <iterator>
 
 Chunk::Chunk() {
+	for (int i = 0; i < 4; i++) {
+		subChunks[i] = new SubChunk();
+	}
+}
+
+Chunk::~Chunk() {
+	for (SubChunk *subChunk : subChunks) {
+		if (subChunk != nullptr) {
+			delete subChunk;
+		}
+	}
 }
 
 
-Chunk::~Chunk() {
+int Chunk::getBlock(collumLoc x, collumLoc y, collumLoc z) {
+	SubChunk *subChunk = subChunks[y >> 4];
+
+	if (subChunk == nullptr) {
+		return 0;
+	}
+	else {
+		return subChunk->getBlock(x, y % 16, z);
+	}
+}
+
+SubChunk::SubChunk() {
+	//std::fill(std::begin(blocks[0][0]), std::end(blocks[15][15]), 1);
+	std::fill(&blocks[0][0][0], &blocks[15][15][15], 1);
+}
+
+int SubChunk::getBlock(relitiveLoc x, relitiveLoc y, relitiveLoc z) {
+	return blocks[x][y][z];
 }
--- CrumbleSrc/Chunk.h
+++ CrumbleSrc/Chunk.h
@@ -1,7 +1,25 @@
 #pragma once
+
+typedef int collumLoc;
+typedef int relitiveLoc;
+
+class SubChunk {
+public:
+	int blocks[16][16][16] = {0};
+public:
+	SubChunk();
+
+	int getBlock(relitiveLoc x, relitiveLoc y, relitiveLoc z);
+};
+
 class Chunk {
+public:
+	SubChunk *subChunks[16] = {};
+
 public:
 	Chunk();
 	~Chunk();
+
+	int getBlock(collumLoc x, collumLoc y, collumLoc z);
 };
 
